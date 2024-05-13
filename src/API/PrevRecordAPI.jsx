dd48328b6f2cc761871f89607042d0f51bef3c80
Pawan Singhania <pawansinghania001@gmail.com>
2024-05-14 02:07:24 +0530

    fix token

--- src/API/PrevRecordAPI.jsx
+++ src/API/PrevRecordAPI.jsx
@@ -8,7 +8,7 @@ const prevRecodApi = createApi({
   reducerPath:'prevRecordApi',
   baseQuery: fetchBaseQuery({ baseUrl: BACKEND_URL,
     prepareHeaders: (headers) => {
-      const authToken = "Bearer ZXlKaGJHY2lPaUpTVXpJMU5pSjkuZXlKemRXSWlPaUk1TWpVeUlpd2laRzl0WVdsdUlqb2lRMElpTENKd2FXUWlPamt6TURJc0ltOXBaQ0k2TmpFMU1Td2laWGh3SWpveE56RTBPREE0TmpFMExDSjFkV2xrSWpvaVpEZ3lOV00zWWpNdE16VXpaQzAwTVRrM0xUa3lNRFl0WlRJNFpXVmxNMlJpTTJZeklpd2lhV0YwSWpveE56RXlNakUyTmpFMExDSnFkR2tpT2lJME16YzFZamhqWmkxak1EUmxMVFEyWVRJdFlUQm1NeTB3TlRJelptSXhZamhsTURZaWZRLlNxa3pWaEJnQ3piM0pqMm04dVZDb04weGZxWWE2NFg2ZkxHdkpOc3NlUW5sT0hhVml2dFh5TDRSME5YZFNOSzh1MXVwLWdpd1JGLXVUY0FhRmRCX2l0amV5cldHb0hZOEJMMUNBRFhaUEpHQ1dzanVmVmdSTnI3cnNLd3dHdWxUNzcyWUtJMUx6LWJiQWk2WVZkVHVkXzRJZmw4THBLRHBFZF9mMkJoMG9jWHFWYW04Q3FydndzWk1vVVpJa1liazFFZzVFSWpBR1IzLUxSZjF1cktwWVRjSDRQanVMT3dqdFZmcXl0WmZfRG9BdmZKZElaanJpN1g4a0VBVmFFWHozWEtGYnRvZElMVmo5Q0pCZkk4QnJJbTA5TDluRmJpRHdRYVdBVmkxd1Z0bTJIa1l3M2xjWEZVQTJCSTlZNGYtTmpweHVLbzdzRXdud2ZidS1LOHJsUQ=="
+      const authToken = "Bearer token-goes-here"
       if (authToken) {
         console.log("headers",headers);
         headers.set('Authorization', authToken);
--- src/API/uploadVideo.js
+++ src/API/uploadVideo.js
@@ -7,7 +7,7 @@ const uploadVideo = createApi({
     baseQuery: fetchBaseQuery({
         baseUrl: BACKEND_URL,
         prepareHeaders: (headers) => {
-            const authToken = "Bearer ZXlKaGJHY2lPaUpTVXpJMU5pSjkuZXlKemRXSWlPaUk1TWpVeUlpd2laRzl0WVdsdUlqb2lRMElpTENKd2FXUWlPamt6TURJc0ltOXBaQ0k2TmpFMU1Td2laWGh3SWpveE56RXpNVGc0TWpBMkxDSjFkV2xrSWpvaVpEZ3lOV00zWWpNdE16VXpaQzAwTVRrM0xUa3lNRFl0WlRJNFpXVmxNMlJpTTJZeklpd2lhV0YwSWpveE56RXdOVGsyTWpBMkxDSnFkR2tpT2lKbE1ETmlOVEUzWkMweVl6UTVMVFF5WVRJdE9EWmhZeTFsTjJVMU1HTTFNRE0zWWpZaWZRLlUyS2laWExWU3pWcmF6LXgydEVlYzJ5eWw5cW42bUxIVlNTS201QTdLakJJOE1HQTZmdC16R2pzbEFSbEFjVlkwTVNLVEJmUE9GdHIxc3JvUGpsLVVQcVlENWEtTkV0YXRFdEJONWlITGoxRnZmVWJLOGN3ZGkzRDAxSDJPbmtXUjc2TmFVQml1bmpfS0VJUFpIYnJtbG5qanpBczIxWC1oaEFONGdiR3Z0TmVTV2tHTGlqVG05NEt2T24wV3VnT3NlREFmZDJDVmZ2TWlxVzRQZ0xTRVhzM2JqTER2d3FXbnhQN3R5MU9xaGNUWnFaM0k0WUtaa3hSdkthSVA1WWprTmozWVRJRklUd1JvTmt3M3p2NXBtd3V6YXNfRThhalhVSzg1bVFGb0tUOFdaRnRWRGtJV1o0MXh6cFQ0dGNNWFZiX3NIOWY0NG9LLTZKNjlQVVJTQQ"
+            const authToken = "Bearer token-goes-here"
             // const authToken = `Bearer ${localStorage.getItem("__Secure-next-auth.session-token")}`;
             if (authToken) {
                 // console.log("headers",headers);
